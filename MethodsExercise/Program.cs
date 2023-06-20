46e8b7d8d76b524e590ff860b3091379aa001f44
rsmith1631 <rsmith1631@gmail.com>
2023-06-20 16:27:56 -0700

    Updated with Sum and Multiply

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 using System.Security.Cryptography.X509Certificates;
 
 namespace MethodsExercise
 {
- class Program
+    class Program
     {
+        public static int Sum(int num1, int num2)
+        {
+            var answer = num1 + num2;
+            return answer;
+        }
+
+        public static int Multiply(int num1, int num2, int num3)
+        {      var amount = num1 * num2 * num3;
+               return amount;
+
+        }
+
         static void Main(string[] args)
         {
+
+            var amountOfCars = Sum(2, 6);
+            var amount = Multiply(55, 37, 21);
+
             Console.WriteLine("What is your first name?");
              var userName = Console.ReadLine(); // input
 
