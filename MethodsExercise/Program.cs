72d4e91a4cc5859ebb6ecc482ea23ab2311ba924
rsmith1631 <rsmith1631@gmail.com>
2023-06-20 15:13:44 -0700

    Added Method questions for site visitors

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace MethodsExercise
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MethodsExercise
 {
-    public class Program
+ class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your first name?");
+             var userName = Console.ReadLine(); // input
+
+            Console.WriteLine($"Hi {userName} What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine("That's awesome! What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("That's great! Lastly what is your favorite band?");
+            var band = Console.ReadLine();
+
+            Console.WriteLine("That's great! Thank you for taking time to fill out our form!");
         }
     }
 }
